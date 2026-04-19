a73a519b83e536416c641aa57622f33953ab1c65
El Mahjoub Bounhar <mahjoubbounhar@gmail.com>
2026-04-19 06:22:52 +0100

    feat: add subtract function

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -1,4 +1,4 @@
-const { add, multiply, factorial } = require('../src/math'); 
+const { add, multiply, factorial, subtract} = require('../src/math'); 
   
 describe('add', () => {   test('additionne deux nombres positifs', () => { 
     expect(add(2, 3)).toBe(5); 
